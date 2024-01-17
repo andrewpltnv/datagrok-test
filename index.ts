195170a552834b19e88146f1ea663afb99f15c7e
andrewpltnv <andrewpltnv@gmail.com>
2024-01-17 03:50:04 +0200

    class

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("asd");
+class NodeClass {
+	constructor(
+		public value: number,
+		public left?: NodeClass,
+		public right?: NodeClass,
+	) {}
+}
+
+const tree: NodeClass = new NodeClass(
+	12,
+	new NodeClass(12, null, new NodeClass(10)),
+);
+
+console.log({ tree });
