52b0b9acd3ea6bda272fedb41e0309a903a41de6
andrewpltnv <andrewpltnv@gmail.com>
2024-01-17 12:51:46 +0200

    refactoring

--- src/second/index.ts
+++ src/second/index.ts
@@ -1,45 +1,40 @@
 // Point[0] < Point[1] // [-25.2,-10.5] | [-10,28] | and so on
 type Point = [number, number];
 
-type Rect = {
-	x: Point;
-	y: Point;
-};
+function isBetween(searchOne: number, [a0, a1]: Point): boolean {
+	return searchOne > a0 && searchOne < a1;
+}
 
 function findPointInCommon(origin: Point, comp: Point) {
 	let resultPoint: Point;
-	if (origin[0] > comp[0] && origin[0] < comp[1]) {
+	if (isBetween(origin[0], comp)) {
 		resultPoint = [origin[0], comp[1]];
-	} else if (origin[1] > comp[0] && origin[1] < comp[0]) {
+	} else if (isBetween(origin[1], comp)) {
 		resultPoint = [comp[0], origin[1]];
 	}
 	return resultPoint;
 }
-
 class RectClass {
 	width;
 	height;
 	constructor(
-		public x: Rect["x"],
-		public y: Rect["y"],
+		public x: Point,
+		public y: Point,
 	) {
 		this.width = x[1] - x[0];
 		this.height = y[1] - y[0];
 	}
-
 	area() {
 		return this.height * this.width;
 	}
-
-	intersectionRect(rect: RectClass) {
+	intersectionArea(rect: RectClass) {
 		const cx = findPointInCommon(rect.x, this.x);
 		const cy = findPointInCommon(rect.y, this.y);
-
-		const commonRect = new RectClass(cx, cy);
-		return commonRect;
+		return new RectClass(cx, cy).area();
 	}
 }
 
-const rect1 = new RectClass([-10, 10], [-5, 15]);
-const rect2 = new RectClass([0, 15], [0, 23]);
-console.log(rect1.intersectionRect(rect2).area());
+const rect1 = new RectClass([-10, 10.3], [-5, 15]);
+const rect2 = new RectClass([0, 15], [-3.9, 23.7]);
+console.log(rect1.intersectionArea(rect2));
+console.log(rect2.intersectionArea(rect1));
